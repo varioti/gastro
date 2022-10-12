8e883488d7cbbbd6c19fef7a402b9231eed572a1
valatt19 <valatt19@gmail.com>
2022-10-12 22:26:16 +0200

    try things with menus

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,10 +14,12 @@ object Main {
       )
       println((new MenuComposer(products, input.toInt)).compose)
     }
-
 }
 
 class MenuComposer(products: List[Product], nb_kcal:Int) {
+
+  // ANONYMOUS FUNCTIONS
+  var isApproximate = (value:Float, refvalue:Float, approximation:Float) => (value <= refvalue + approximation && value >= refvalue - approximation)
 
   private def getRandomProducts(howMany: Int): List[Product] = {
     products.length match {
@@ -25,25 +27,54 @@ class MenuComposer(products: List[Product], nb_kcal:Int) {
         for { _ <- List.range(0, howMany) } yield products(Random.between(0, n))
       case _ => Nil
     }
+  }
 
+  private def getBalancedProducts(): List[Product] = {
+    products.length match {
+      case n if n > 0 =>
+        // COMPREHENSION LIST
+        for {
+          product <- products
+          totalNutr = product.protein + product.glucides + product.lipides 
+          if (isApproximate(product.protein/totalNutr,0.25.toFloat,0.1.toFloat) && 
+            isApproximate(product.glucides/totalNutr,0.4.toFloat,0.1.toFloat) && 
+            isApproximate(product.lipides/totalNutr,0.35.toFloat,0.05.toFloat)) 
+        } yield product
+      case _ => Nil
+    }
+  }
+
+  private def getMenu(nb_kcal: Int): List[List[Product]] = {
+    val menus = products.combinations(2).toList
+    for {
+          menu <- menus
+          totalEnergy = (menu.map(_.energy).foldLeft(0.0)(_ + _)).toFloat
+          totalProt = (menu.map(_.protein).foldLeft(0.0)(_ + _)).toFloat
+          totalGluc = (menu.map(_.glucides).foldLeft(0.0)(_ + _)).toFloat
+          totalLip = (menu.map(_.lipides).foldLeft(0.0)(_ + _)).toFloat
+          totalNutr = totalProt + totalGluc + totalLip 
+          if (isApproximate(totalEnergy,nb_kcal,25.toFloat) &&
+            isApproximate(totalProt/totalNutr,0.25.toFloat,0.05.toFloat) && 
+            isApproximate(totalGluc/totalNutr,0.4.toFloat,0.05.toFloat) && 
+            isApproximate(totalLip/totalNutr,0.35.toFloat,0.05.toFloat))
+        } yield menu
   }
 
   def compose: String = {
-    val randomProducts = getRandomProducts(4)
+    val balancedProducts = getBalancedProducts()
+    val menu = getMenu(nb_kcal).head
     println("----- I will try assembling the following products:")
-    (0 until 3).foreach(i => println(f"--- $i --- " + randomProducts(i)))
+    (0 until menu.length).foreach(i => println(f"--- $i --- " + menu(i)))
 
-    if (randomProducts.map(_.energy).foldLeft(0.0)(_ + _) >= nb_kcal)
-      "The 4 selected products are at least --- kcal"
+    val total_energy = (menu.map(_.energy).foldLeft(0.0)(_ + _)).toFloat
+    if (true)
+      "The 4 selected products have a total of :" + total_energy.toString + "kcal"
     else
-      "The 4 selected products are less than --- kcal"
+      "The 4 selected products are not " + nb_kcal.toString + "kcal, but " + total_energy.toString + "kcal"
   }
 }
 
-case class Product(id: Int, name: String, new_energy: Int, new_protein: Float) {
-
-  def energy: Int = new_energy
-  def protein : Float = new_protein
+case class Product(id: Int, name: String, energy: Int, protein: Float,  glucides: Float, lipides: Float) {
 
   override def toString: String = f"$name (${id.toString})"
 }
@@ -59,7 +90,9 @@ object GastroExtractor {
         cols(0).toInt,
         cols(1),
         cols(4).toInt,
-        cols(5).toFloat
+        cols(5).toFloat,
+        cols(6).toFloat,
+        cols(9).toFloat
       )).toList
     }.toEither
 
